66af2cf73d5084761c0a8b7aa269092984e0b604
David da Rocha Fernandes <davidrfernandes03@gmail.com>
2024-11-28 14:49:26 +0000

    Enhaced TPA

--- scripts/filtered-temples.js
+++ scripts/filtered-temples.js
@@ -19,70 +19,70 @@ const temples = [
         location: "Aba, Nigeria",
         dedicated: "2005, August, 7",
         area: 11500,
-        imageUrl: "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/aba-nigeria/400x250/aba-nigeria-temple-lds-273999-wallpaper.jpg"
+        imageUrl: "images/w4-site/filt-temple-01.avif"
     },
     {
         templeName: "Manti Utah",
         location: "Manti, Utah, USA",
         dedicated: "1888, May, 21",
         area: 74792,
-        imageUrl: "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/manti-utah/400x250/manti-temple-768192-wallpaper.jpg"
+        imageUrl: "images/w4-site/filt-temple-02.avif"
     },
     {
         templeName: "Payson Utah",
         location: "Payson, Utah, USA",
         dedicated: "2015, June, 7",
         area: 96630,
-        imageUrl: "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/payson-utah/400x225/payson-utah-temple-exterior-1416671-wallpaper.jpg"
+        imageUrl: "images/w4-site/filt-temple-03.avif"
     },
     {
         templeName: "Yigo Guam",
         location: "Yigo, Guam",
         dedicated: "2020, May, 2",
         area: 6861,
-        imageUrl: "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/yigo-guam/400x250/yigo_guam_temple_2.jpg"
+        imageUrl: "images/w4-site/filt-temple-04.avif"
     },
     {
         templeName: "Washington D.C.",
         location: "Kensington, Maryland, USA",
         dedicated: "1974, November, 19",
         area: 156558,
-        imageUrl: "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/washington-dc/400x250/washington_dc_temple-exterior-2.jpeg"
+        imageUrl: "images/w4-site/filt-temple-05.avif"
     },
     {
         templeName: "Lima Perú",
         location: "Lima, Perú",
         dedicated: "1986, January, 10",
         area: 9600,
-        imageUrl: "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/lima-peru/400x250/lima-peru-temple-evening-1075606-wallpaper.jpg"
+        imageUrl: "images/w4-site/filt-temple-06.avif"
     },
     {
         templeName: "Mexico City Mexico",
         location: "Mexico City, Mexico",
         dedicated: "1983, December, 2",
         area: 116642,
-        imageUrl: "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/mexico-city-mexico/400x250/mexico-city-temple-exterior-1518361-wallpaper.jpg"
+        imageUrl: "images/w4-site/filt-temple-07.avif"
     },
     {
         templeName: "São Paulo Brazil",
         location: "São Paulo, Brazil",
         dedicated: "1978, October, 30",
         area: 12000,
-        imageUrl: "images/filt-temple-01.avif"
+        imageUrl: "images/w4-site/filt-temple-08.avif"
     },
     {
         templeName: "Rome Italy",
         location: "Rome, Italy",
         dedicated: "2019, March, 10",
         area: 12345,
-        imageUrl: "images/filt-temple-02.avif"
+        imageUrl: "images/w4-site/filt-temple-09.avif"
     },
     {
         templeName: "Salt Lake City",
         location: "Salt Lake City, Utah, USA",
         dedicated: "1893, April, 6",
         area: 253000,
-        imageUrl: "images/filt-temple-03.avif"
+        imageUrl: "images/w4-site/filt-temple-10.avif"
     }
 ];
 
@@ -177,4 +177,4 @@ navItems.forEach(item => {
 document.getElementById("current-year").textContent = new Date().getFullYear();
 
 // Get last modification
-document.getElementById("lastModified").textContent = `Last Modification: ${document.lastModified}`
+document.getElementById("lastModified").textContent = `Last Modification: ${document.lastModified}`;
